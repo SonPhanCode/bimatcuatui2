ccdbfd0bf0e12f88bd1c39c2542825f3ea596e61
Sơn Phan <87313732+SonPhanCode@users.noreply.github.com>
2021-07-17 14:22:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ const CONFIG = {
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    Đằng sau chữ yêu đây là thương
+    Nhấn Ok để tiếp tục`,
+    btnIntro: 'Ok',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
@@ -15,5 +16,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/son.phan2007' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
